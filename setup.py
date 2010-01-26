3157ea053c2334f9362fb566690cfc0afa004c51
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2010-01-26 12:22:58 +0000

    Initial commit of the osha.fop product. This contains the code for the FOP / Focal Point/ OSHNetwork area of OSHA.

---
+++ setup.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Focal Point specific views, also known as the OSHNetwork section
+"""
+from setuptools import setup, find_packages
+import os
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.0'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('src', 'osha', 'theme', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' 
+    )
+
+tests_require=['zope.testing']
+
+setup(name='osha.fop',
+      version=version,
+      description="Focal Point specific views, also known as the OSHNetwork section",
+      long_description=long_description
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "License :: OSI Approved :: European Union Public Licence 1.1 (EUPL 1.1)",
+        ],
+      keywords='osha plone fop',
+      author='Syslab.com GmbH',
+      author_email='info@syslab.com',
+      url='https://svn.syslab.com/svn/OSHA/osha.fop',
+      license='GPL + EUPL',
+      packages=['osha', 'osha/fop'],
+      namespace_packages=['osha'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'osha.policy',
+      ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'osha.fop.tests.test_suite',
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
