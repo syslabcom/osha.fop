eee5406b313f4200d483427149f050b4149fbdcb
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2010-04-07 16:17:39 +0000

    Preparing for 1.0 release, added translations to the FOP link section headings.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.10dev'
+version = '1.0'
 
 long_description = (
     read('README.txt')
