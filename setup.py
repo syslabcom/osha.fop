4ec72227f7f361f8341bffc1b64c50496729439e
deroiste <deroiste@97d050b9-b799-4550-a901-4f04598d4601>
2010-11-09 15:33:26 +0000

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.1dev'
+version = '1.1'
 
 long_description = (
     read('README.txt')
